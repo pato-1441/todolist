f1092c8cc62b52a2cf107ae8c2706a161fb37cb1
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-08-16 19:34:37 -0300

    arreglo de estilos, funcionalidad agregar tareas, alerta con timeout

--- src/app.js
+++ src/app.js
@@ -1,17 +1,35 @@
+function creoID() { return parseInt(Math.random() * 10000) }
+
 const inputAdd = document.getElementById('inputAdd');
 const buttonAdd = document.getElementById('buttonAdd');
 buttonAdd.addEventListener('click',crearElemento);
 
-
-const tareas = []
+const contenedorTareas = document.getElementById('tareas');
+const tareas = [];
 
 tareas.push('Hola','Chau');
 
-tareas.forEach(el => {
-    const tarea = document.getElementById('elementoUno');
-    tarea.innerHTML=`${tareas[0]}`
-});
-
 function crearElemento(){
-    console.log(inputAdd.value);
+    let tarea = inputAdd.value;
+    if(inputAdd.value!==''){
+        tareas.push(tarea);
+        let tareaARealizar = document.createElement("p");
+        //tareaARealizar.innerHTML = `${tareas[tareas.length-1]}`
+        tareaARealizar.innerHTML = `<div class="bg-gray-200 my-2 p-1 rounded-xl flex items-center justify-between">
+                                        <p class="ml-2 mr-1">${tareas[tareas.length-1]}</p>
+                                        <button class="btn btn-xs btn-circle btn-outline mr-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                                        </button>
+                                    </div>`
+        contenedorTareas.append(tareaARealizar);
+        contenedorTareas.classList.add('p-2');
+        inputAdd.value='';
+    } else {
+        const alertError = document.getElementById('alertError');
+        alertError.classList.remove('hidden')
+        setTimeout(function(){
+            alertError.classList.add('hidden');
+        }, 5000);
+        inputAdd.focus();
+    }
 }
