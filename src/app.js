9f44ed895cce040ecc0284506f25ac6a05db081b
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-08-22 20:45:35 -0300

    add localstorage e IDs tareas

--- src/app.js
+++ src/app.js
@@ -1,22 +1,29 @@
-function creoID() { return parseInt(Math.random() * 10000) }
+class Tarea{
+    constructor(tarea,id) {
+        this.tarea = tarea;
+        this.id = id;        
+    }
+}
 
 const inputAdd = document.getElementById('inputAdd');
 const buttonAdd = document.getElementById('buttonAdd');
-buttonAdd.addEventListener('click',crearElemento);
+buttonAdd.addEventListener('click',()=>{crearElemento()});
 
 const contenedorTareas = document.getElementById('tareas');
 const tareas = [];
+localStorage.setItem('tareasUsuario',JSON.stringify(tareas));
 
-tareas.push('Hola','Chau');
-
-function crearElemento(){
+const crearElemento = () => {
+    const creoID=()=>{return parseInt(Math.random()*10000)}
     let tarea = inputAdd.value;
-    if(inputAdd.value!==''){
-        tareas.push(tarea);
+    let tareaID = creoID();
+    if(inputAdd.value!==''){        
+        let tareaObjeto=new Tarea(tarea,tareaID);
+        tareas.push(tareaObjeto);
+        localStorage.setItem('tareasUsuario',JSON.stringify(tareas));
         let tareaARealizar = document.createElement("p");
-        //tareaARealizar.innerHTML = `${tareas[tareas.length-1]}`
         tareaARealizar.innerHTML = `<div class="bg-gray-200 my-2 p-1 rounded-xl flex items-center justify-between">
-                                        <p class="ml-2 mr-1">${tareas[tareas.length-1]}</p>
+                                        <p class="ml-2 mr-1">${tareas[tareas.length-1].tarea}</p>
                                         <button class="btn btn-xs btn-circle btn-outline mr-2">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
                                         </button>
@@ -29,7 +36,7 @@ function crearElemento(){
         alertError.classList.remove('hidden')
         setTimeout(function(){
             alertError.classList.add('hidden');
-        }, 5000);
+        }, 10000);
         inputAdd.focus();
     }
 }
