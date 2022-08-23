ab60f22292dea8bafebea3d39a04d555e01205d9
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-08-23 12:57:14 -0300

    working on delete tareas

--- src/app.js
+++ src/app.js
@@ -7,32 +7,22 @@ class Tarea{
 
 const inputAdd = document.getElementById('inputAdd');
 const buttonAdd = document.getElementById('buttonAdd');
-buttonAdd.addEventListener('click',()=>{crearElemento()});
+buttonAdd.addEventListener('click',()=>{crearTarea()});
 
 const contenedorTareas = document.getElementById('contenedorTareas');
 const tareas = [];
 localStorage.setItem('tareasUsuario',JSON.stringify(tareas));
+let contadorID = 0;
 
-const crearElemento = () => {
-    const creoID=()=>{return parseInt(Math.random()*10000)}
+const crearTarea = () => {
     let tarea = inputAdd.value;
-    let tareaID = creoID();
-    const fragment = document.createDocumentFragment();
-    if(inputAdd.value!==''){        
+    let tareaID = contadorID;    
+    if(inputAdd.value!==''){    
+        contadorID++;    
         let tareaObjeto=new Tarea(tarea,tareaID);
         tareas.push(tareaObjeto);
-        localStorage.setItem('tareasUsuario',JSON.stringify(tareas));
-        let tareaARealizar = document.createElement("p");
-        tareaARealizar.innerHTML = `<div class="bg-gray-200 my-2 p-1 rounded-xl flex items-center justify-between">
-                                        <p class="ml-2 mr-1">${tareas[tareas.length-1].tarea}</p>
-                                        <button class="btn btn-xs btn-circle btn-outline mr-2" id="${tareaID}">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
-                                        </button>
-                                    </div>`;
-        fragment.appendChild(tareaARealizar);
-        //contenedorTareas.append(tareaARealizar);
-        contenedorTareas.appendChild(fragment);
-        contenedorTareas.classList.add('px-2','py-1','shadow-lg');
+        localStorage.setItem('tareasUsuario',JSON.stringify(tareas));        
+        actualizarTareas();
         inputAdd.value='';
     } else {
         const alertError = document.getElementById('alertError');
@@ -44,10 +34,25 @@ const crearElemento = () => {
     }
 }
 
-const borrarElemento = () =>{
-    let i = 0;
-    tareas.forEach(tarea => {
-        const borrarTarea = document.getElementById(tarea[i].id)
-    });
-    i++;
+const eliminarTarea = () => {
+    let tareasLocal = JSON.parse(localStorage.getItem('tareasUsuario'));
+    tareasLocal
+    let btnEliminarTarea = document.getElementById(`eliminarBtn${3}`);
+
+    actualizarTareas();
+};
+
+const actualizarTareas = () => {
+    const fragmentTarea = document.createDocumentFragment();
+    let tareaARealizar = document.createElement("p");
+    tareaARealizar.innerHTML = `<div class="bg-white my-2 p-1 rounded-xl flex items-center justify-between">
+                                    <p class="ml-2 mr-1">${tareas[tareas.length-1].tarea}</p>
+                                    <button class="btn btn-xs btn-circle btn-outline mr-2" id="eliminarBtn${contadorID}">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                                    </button>
+                                </div>`;
+    fragmentTarea.appendChild(tareaARealizar);
+    //contenedorTareas.append(tareaARealizar);
+    contenedorTareas.appendChild(fragmentTarea);
+    contenedorTareas.classList.add('px-2','py-1','shadow-lg');
 }
