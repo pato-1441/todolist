1db1528ca30e98a3180a41f752ad62671d3148e4
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-08-23 09:27:30 -0300

    add logo dropdown en nav, color al input

--- src/app.js
+++ src/app.js
@@ -17,20 +17,22 @@ const crearElemento = () => {
     const creoID=()=>{return parseInt(Math.random()*10000)}
     let tarea = inputAdd.value;
     let tareaID = creoID();
+    const fragment = document.createDocumentFragment();
     if(inputAdd.value!==''){        
         let tareaObjeto=new Tarea(tarea,tareaID);
         tareas.push(tareaObjeto);
         localStorage.setItem('tareasUsuario',JSON.stringify(tareas));
-        //muestro en pantalla la tarea
         let tareaARealizar = document.createElement("p");
         tareaARealizar.innerHTML = `<div class="bg-gray-200 my-2 p-1 rounded-xl flex items-center justify-between">
                                         <p class="ml-2 mr-1">${tareas[tareas.length-1].tarea}</p>
                                         <button class="btn btn-xs btn-circle btn-outline mr-2" id="${tareaID}">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
                                         </button>
-                                    </div>`
-        contenedorTareas.append(tareaARealizar);
-        contenedorTareas.classList.add('p-2');
+                                    </div>`;
+        fragment.appendChild(tareaARealizar);
+        //contenedorTareas.append(tareaARealizar);
+        contenedorTareas.appendChild(fragment);
+        contenedorTareas.classList.add('px-2','py-1','shadow-lg');
         inputAdd.value='';
     } else {
         const alertError = document.getElementById('alertError');
@@ -43,4 +45,9 @@ const crearElemento = () => {
 }
 
 const borrarElemento = () =>{
+    let i = 0;
+    tareas.forEach(tarea => {
+        const borrarTarea = document.getElementById(tarea[i].id)
+    });
+    i++;
 }
